70976b42a77aa7b00e4c4df668d48bf9d0ec681d
Freddy Jarva <freddy.a.jarva@gmail.com>
2021-12-16 12:16:27 +0100

    add optional metadata fields 'notes' and 'name'

--- src/lib.rs
+++ src/lib.rs
@@ -221,31 +221,28 @@ fn write_metadata_to_csv(
     permalink: String,
     meta_data: request::MetaData,
 ) -> Result<(), anyhow::Error> {
+    const NONE_STR: &'static str = "None";
     f.write_all(format!("# rom_build {}\n", meta_data.build).as_bytes())?;
     f.write_all(format!("# permalink {}\n", permalink).as_bytes())?;
+    f.write_all(
+        format!(
+            "# name {}\n",
+            meta_data.name.unwrap_or(NONE_STR.to_string())
+        )
+        .as_bytes(),
+    )?;
     f.write_all(format!("# goal {}\n", meta_data.goal).as_bytes())?;
     f.write_all(format!("# mode {}\n", meta_data.mode).as_bytes())?;
     f.write_all(format!("# rom_mode {}\n", meta_data.rom_mode).as_bytes())?;
-    f.write_all(
-        format!(
-            "# entry_crystals_ganon {}\n",
-            meta_data.entry_crystals_ganon
-        )
-        .as_bytes(),
-    )?;
-    f.write_all(
-        format!(
-            "# entry_crystals_tower {}\n",
-            meta_data.entry_crystals_tower
-        )
-        .as_bytes(),
-    )?;
-    f.write_all(format!("# item_functionality {}\n", meta_data.item_functionality).as_bytes())?;
-    f.write_all(format!("# item_placement {}\n", meta_data.item_placement).as_bytes())?;
-    f.write_all(format!("# item_pool {}\n", meta_data.item_pool).as_bytes())?;
     f.write_all(format!("# logic {}\n", meta_data.logic).as_bytes())?;
     f.write_all(format!("# accessibility {}\n", meta_data.accessibility).as_bytes())?;
+    f.write_all(format!("# weapons {}\n", meta_data.weapons).as_bytes())?;
+    f.write_all(format!("# spoilers {}\n", meta_data.spoilers).as_bytes())?;
+    f.write_all(format!("# tournament {}\n", meta_data.tournament).as_bytes())?;
     f.write_all(format!("# dungeon_items {}\n", meta_data.dungeon_items).as_bytes())?;
+    f.write_all(format!("# item_pool {}\n", meta_data.item_pool).as_bytes())?;
+    f.write_all(format!("# item_placement {}\n", meta_data.item_placement).as_bytes())?;
+    f.write_all(format!("# item_functionality {}\n", meta_data.item_functionality).as_bytes())?;
     f.write_all(
         format!(
             "# enemizer_boss_shuffle {}\n",
@@ -281,6 +278,24 @@ fn write_metadata_to_csv(
         )
         .as_bytes(),
     )?;
+    f.write_all(
+        format!(
+            "# entry_crystals_ganon {}\n",
+            meta_data.entry_crystals_ganon
+        )
+        .as_bytes(),
+    )?;
+    f.write_all(
+        format!(
+            "# entry_crystals_tower {}\n",
+            meta_data.entry_crystals_tower
+        )
+        .as_bytes(),
+    )?;
+    f.write_all(format!("# allow_quickswap {}\n", meta_data.tournament).as_bytes())?;
+    f.write_all(format!("# worlds {}\n", meta_data.worlds).as_bytes())?;
+    f.write_all(format!("# world_id {}\n", meta_data.world_id).as_bytes())?;
+    f.write_all(format!("# notes {}\n", meta_data.notes.unwrap_or(NONE_STR.to_string())).as_bytes())?;
     Ok(())
 }
 

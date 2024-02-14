8935ac3eb56b0081b18aa5462172b6c056e38ac7
fj <freddy.a.jarva@gmail.com>
2024-02-14 15:38:05 +0100

    remove unused trait

--- src/time.rs
+++ src/time.rs
@@ -135,26 +135,3 @@ where
     }
 }
 
-pub trait SequenceStatistics {
-    fn avg(&self) -> Duration;
-    fn rolling_avg(&self, n: usize) -> Duration;
-}
-
-impl SequenceStatistics for Vec<Duration> {
-    fn avg(&self) -> Duration {
-        let sum: i64 = self.iter().map(|&d| d.num_milliseconds()).sum();
-        let avg = sum / (self.len() as i64);
-        Duration::milliseconds(avg)
-    }
-
-    fn rolling_avg(&self, n: usize) -> Duration {
-        let sum: i64 = self
-            .iter()
-            .rev()
-            .take(n)
-            .map(|&d| d.num_milliseconds())
-            .sum();
-        let avg = sum / (usize::min(n, self.len()) as i64);
-        Duration::milliseconds(avg)
-    }
-}

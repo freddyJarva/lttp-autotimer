ec8b690d47916973a5afd016dae8e86802cb4c18
fj <freddy.a.jarva@gmail.com>
2024-02-26 22:39:38 +0100

    tauri migration: add tauri

---
+++ frontend/src-tauri/src/main.rs
@@ -0,0 +1,77 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::Manager;
+use tokio::sync::{mpsc, Mutex};
+
+struct InputTx {
+    inner: Mutex<mpsc::Sender<String>>
+}
+
+async fn async_process_model(
+    mut input_rx: mpsc::Receiver<String>,
+    output_tx: mpsc::Sender<String>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    loop {
+        while let Some(input) = input_rx.recv().await {
+            let output = input;
+            output_tx.send(output).await?;
+        }
+    }
+}
+
+#[tauri::command]
+fn rs2js<R: tauri::Runtime>(message: String, manager: &impl Manager<R>) {
+    println!("rs2js {}", message);
+    manager
+        .emit_all("rs2js", format!("rs: {}", message))
+        .unwrap();
+}
+
+
+#[tauri::command]
+async fn js2rs(
+    message: String,
+    state: tauri::State<'_, InputTx>,
+) -> Result<(), String> {
+    println!("{:?}", message);
+    let in_tx = state.inner.lock().await;
+    in_tx.send(message).await.map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[tokio::main]
+async fn main() {
+    tauri::async_runtime::set(tokio::runtime::Handle::current());
+    let (in_tx, in_rx) = mpsc::channel::<String>(1);
+    let (out_tx, mut out_rx) = mpsc::channel::<String>(1);
+
+  tauri::Builder::default()
+    .manage(InputTx {
+        inner: Mutex::new(in_tx),
+    })
+    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![js2rs])
+    .setup(|app| {
+        tauri::async_runtime::spawn(async move {
+            async_process_model(in_rx, out_tx).await
+        });
+
+        let app_handle = app.handle();
+        tauri::async_runtime::spawn(async move {
+            loop {
+                if let Some(output) = out_rx.recv().await {
+                    rs2js(output, &app_handle);
+                }
+            }
+        });
+
+        Ok(())
+    })
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+    import { onMount } from 'svelte'
+    import { listen } from '@tauri-apps/api/event'
+    import { invoke } from '@tauri-apps/api/tauri'
+
+    /** 
+     * @typedef {Object} Message
+     * @property {number} timestamp
+     * @property {string} message
+     */
+
+    let unlisten;
+
+    let output = ''
+    /** @type {Message[]} outputs */
+    let outputs = []
+    /** @type {Message[]} inputs */
+    let inputs = []
+    let name = ''
+    let greetMsg = ''
+
+
+    async function sendOutput() {
+        console.log("THIS IS SVELTE js2rs: " + output)
+        outputs = [...outputs, { timestamp: Date.now(), message: output }]
+        invoke('js2rs', { message: output })
+    }
+
+    onMount(async () => {
+        unlisten = await listen('rs2js', (event) => {
+            console.log("THIS IS SVELTE rs2js: " + event.payload)
+            let input = event.payload
+            inputs = [...inputs, {timestamp: Date.now(), message: input}]
+        })
+    })
+
+    async function greet() {
+        greetMsg = await invoke('greet', { name })
+    }
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div style="display:grid; grid-template-columns: auto auto;">
+    <input id="greet-input" placeholder="Enter a name.." bind:value="{name}" />
+    <button on:click="{greet}">Greet</button>
+    <p>{greetMsg}</p>
+
+    <div style="grid-column: span 2; grid-row: 1;">
+        <label for="messageInput" style="display: block;">
+        <input id="messageInput" bind:value={output}>
+        <br>
+        <button on:click={sendOutput}>Send to Rust</button>
+    </div>
+    <div style="grid-column: 1; grid-row: 2;">
+        <h3>Output</h3>
+        <ol>
+            {#each outputs as output}
+                <li>{output.timestamp}: {output.message}</li>
+            {/each}
+        </ol>
+    </div>
+    <div style="grid-column: 2; grid-row: 2;">
+        <h3>Input</h3>
+        <ol>
+            {#each inputs as inp}
+                <li>{inp.timestamp}: {inp.message}</li>
+            {/each}
+        </ol>
+    </div>
+</div>
+
